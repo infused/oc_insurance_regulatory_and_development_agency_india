93584142d27099583498dffbd152c1888d2d76db
Keith Morrison <keithm@infused.org>
2015-02-08 15:11:00 -0800

    reinsurer scraper

--- scraper.rb
+++ scraper.rb
@@ -6,12 +6,11 @@ require 'open-uri'
 require 'turbotlib'
 require_relative 'scraper/insurance_broker'
 require_relative 'scraper/insurer'
+require_relative 'scraper/reinsurer'
 
 Scraper::InsuranceBroker.scrape
 Scraper::Insurer.scrape
-
-# 'Non-Life Insurers' => 'https://www.irda.gov.in/ADMINCMS/cms/NormalData_Layout.aspx?page=PageNo264&mid=3.2.10'
-# 'Re-Insurers' => 'https://www.irda.gov.in/ADMINCMS/cms/NormalData_Layout.aspx?page=PageNo1687&mid=3.3.6'
+Scraper::Reinsurer.scrape
 
 # 'Third Party Administrators' => 'https://www.irda.gov.in/ADMINCMS/cms/NormalData_Layout.aspx?page=PageNo646&mid=9.5.7'
 
---
+++ scraper/reinsurer.rb
@@ -0,0 +1,48 @@
+module Scraper
+  class Reinsurer
+    URLs = {
+      'Re-Insurers' => 'https://www.irda.gov.in/ADMINCMS/cms/NormalData_Layout.aspx?page=PageNo1687&mid=3.3.6'
+    }
+
+    def self.clean_string(s)
+      clean = s.dup.encode(Encoding::ASCII, invalid: :replace, undef: :replace, replace: '')
+
+      clean.squeeze!(' ')              # remove extra spaces
+      clean.strip!                     # remove leading/trailing whitespace
+      clean.gsub!(/,,/, ',')           # remove double commas
+      clean.gsub!(/,\ ,/, '')          # remove double commas separated by a space
+      clean.gsub!(/^,/, '')            # remove leading commas
+      clean.gsub!(/,$/, '')            # remove trailing commas
+      clean.gsub!(/,[^\ ]/, ', ')      # add space after commas
+      clean.strip
+    end
+
+    def self.scrape
+      URLs.each do |category, url|
+        doc = Nokogiri(open(url))
+
+        doc.search('#tab1 table table table table tr').each do |co|
+          cert_no = co.search('td')[0].inner_text.strip.gsub('.', '') rescue nil
+
+          if cert_no =~ /\A\d{1,2}\Z/
+            _a1, _a2, *address_parts = co.search('td')[1].inner_text.split("\r\n")
+            contact_parts = co.search('td')[3].inner_text.split("\r\n")
+            phone = contact_parts.detect {|s| s =~ /(tele*|phone)/i }.gsub(/[^\d\(\)\.\:]/, '') rescue nil
+
+            data = {
+              cert_no: cert_no,
+              company_name: clean_string(co.search('td')[1].inner_text),
+              principal_officer: clean_string(co.search('td')[2].inner_text.split("\r\n").join(', ')),
+              phone: phone,
+              category: category,
+              source_url: url,
+              sample_date: Time.now
+            }
+
+            puts JSON.dump(data)
+          end
+        end
+      end
+    end
+  end
+end
